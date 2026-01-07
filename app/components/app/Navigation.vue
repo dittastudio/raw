c15713251feb65de24fd2a52d1ef8d3cacf6db9f
Michael Pumo <hello@michaelpumo.com>
2026-01-07 18:14:29 +0000

    Sort navigation logic.

--- app/components/app/Navigation.vue
+++ app/components/app/Navigation.vue
@@ -6,16 +6,24 @@ interface Props {
 }
 
 const { items } = defineProps<Props>()
+const route = useRoute()
 </script>
 
 <template>
-  <nav>
-    <ul>
+  <nav class="bg-black text-white lg:bg-transparent lg:text-black">
+    <ul class="flex items-center justify-center lg:gap-12 flex-col lg:flex-row uppercase font-display text-32 lg:font-mono lg:text-20 font-extrabold">
       <li
         v-for="item in items"
         :key="item._uid"
+        :class="{
+          'lg:font-extrabold': determineHref(item.link).startsWith(route.path) && route.path !== '/',
+          'text-outline-white lg:text-outline-none lg:font-normal': !determineHref(item.link).startsWith(route.path) || route.path === '/',
+        }"
       >
-        <StoryblokLink :item="item.link">
+        <StoryblokLink
+          :item="item.link"
+          class="block"
+        >
           {{ item.title }}
         </StoryblokLink>
       </li>
