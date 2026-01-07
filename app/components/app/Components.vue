b08ee25de97199bc2f752c4c422afaf741353a1e
Michael Pumo <hello@michaelpumo.com>
2026-01-07 01:31:12 +0000

    Setup migration area.

--- app/components/app/Components.vue
+++ app/components/app/Components.vue
@@ -2,19 +2,25 @@
 import type {
   Page,
   Project,
-} from "@@/.storyblok/types/289672313529140/storyblok-components";
+} from '@@/.storyblok/types/289672313529140/storyblok-components'
 
 interface Props {
-  content: Page | Project;
+  content: Page | Project
 }
 
-const { content } = defineProps<Props>();
+const { content } = defineProps<Props>()
 </script>
 
 <template>
   <div>
-    <section v-for="block in content.blocks" :key="block._uid">
-      <BlockText v-if="block.component === 'block_text'" :block="block" />
+    <section
+      v-for="block in content.blocks"
+      :key="block._uid"
+    >
+      <BlockText
+        v-if="block.component === 'block_text'"
+        :block="block"
+      />
     </section>
   </div>
 </template>
