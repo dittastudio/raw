7e592407c4fa4369b5814aa0792f6d622b961d80
Will The Dev <hello@willthe.dev>
2026-01-19 14:16:49 +0000

    hover list WIP

--- app/components/ui/Expandable.vue
+++ app/components/ui/Expandable.vue
@@ -9,7 +9,7 @@ const { isOpen = false } = defineProps<Props>()
 <template>
   <div
     data-component="ui-expand-collapse"
-    class="grid overflow-hidden transition-[grid-template-rows] duration-500 ease-inOutQuart"
+    class="grid overflow-hidden transition-[grid-template-rows] duration-300 ease-inOutQuart"
     :class="{
       'grid-rows-[0fr]': !isOpen,
       'grid-rows-[1fr]': isOpen,
@@ -19,7 +19,7 @@ const { isOpen = false } = defineProps<Props>()
       class="min-h-0 transition-opacity ease-out"
       :class="{
         'opacity-0 duration-200': !isOpen,
-        'opacity-100 duration-500 delay-400': isOpen,
+        'opacity-100 duration-200 delay-200': isOpen,
       }"
     >
       <slot />
--- app/components/ui/List.vue
+++ app/components/ui/List.vue
@@ -15,7 +15,7 @@ const {
 const themeHoverClasses = computed(() => {
   switch (theme) {
     case 'blue':
-      return 'hover:bg-blue [&:hover_.is-outlined]:text-(--app-text-color)'
+      return 'hover:bg-blue'
     case 'green':
       return 'hover:text-green'
     case 'pink':
@@ -26,18 +26,31 @@ const themeHoverClasses = computed(() => {
       return 'hover:text-blue'
   }
 })
+
+const openIndex = ref<number | null>(null)
+
+const toggleItem = (index: number) => {
+  openIndex.value = openIndex.value === index ? null : index
+}
+
+const isScreenMd = useAtMedia(getMediaQuery('md'))
 </script>
 
 <template>
-  <div class="wrapper-maxx">
+  <div class="">
     <ul class="ui-list__list">
       <li
         v-for="(item, index) in items"
         :key="item._uid"
         class="
           ui-list__item
+          group
+          transition-[background-color]
+          duration-200
+          ease-out
         "
         :class="themeHoverClasses"
+        @click="!isScreenMd && toggleItem(index)"
       >
         <div
           class="
@@ -55,17 +68,21 @@ const themeHoverClasses = computed(() => {
               py-4
             "
           >
-            <span class="col-span-3 type-h2 is-outlined">
+            <span class="col-span-1 sm:col-span-2 md:col-span-3 type-h2 text-outline-current transition-[color,-webkit-text-stroke] duration-200 ease-out group-hover:text-offblack cursor-default">
               {{ (index + 1).toString().padStart(2, '0') }}
             </span>
 
-            <h3 class="col-span-5 type-h2 is-outlined">
+            <h3 class="col-span-3 sm:col-span-6 md:col-span-5 type-h2 text-outline-current transition-[color,-webkit-text-stroke] duration-200 ease-out group-hover:text-offblack cursor-default">
               {{ item.title }}
             </h3>
 
-            <p class="col-span-4 type-p">
-              {{ item.copy }}
-            </p>
+            <div class="col-start-2 col-span-3 sm:col-start-3 sm:col-span-6 md:col-span-4">
+              <UiExpandable :is-open="isScreenMd || openIndex === index">
+                <p class="type-p max-md:pt-4">
+                  {{ item.copy }}
+                </p>
+              </UiExpandable>
+            </div>
           </div>
         </div>
       </li>
