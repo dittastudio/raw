dbfd7da5f23a65046042e7fb1cd082b49bcd5b8f
Will The Dev <hello@willthe.dev>
2026-01-19 16:05:30 +0000

    states for hover list added

--- app/components/ui/Expandable.vue
+++ app/components/ui/Expandable.vue
@@ -1,25 +1,26 @@
 <script lang="ts" setup>
 interface Props {
   isOpen?: boolean
+  isDisabled?: boolean
 }
 
-const { isOpen = false } = defineProps<Props>()
+const { isOpen = false, isDisabled = false } = defineProps<Props>()
 </script>
 
 <template>
   <div
     data-component="ui-expand-collapse"
-    class="grid overflow-hidden transition-[grid-template-rows] duration-300 ease-inOutQuart"
     :class="{
-      'grid-rows-[0fr]': !isOpen,
-      'grid-rows-[1fr]': isOpen,
+      'grid overflow-hidden transition-[grid-template-rows] duration-300 ease-inOutExpo': !isDisabled,
+      'grid-rows-[0fr]': !isOpen && !isDisabled,
+      'grid-rows-[1fr]': isOpen && !isDisabled,
     }"
   >
     <div
-      class="min-h-0 transition-opacity ease-out"
       :class="{
-        'opacity-0 duration-200': !isOpen,
-        'opacity-100 duration-200 delay-200': isOpen,
+        'min-h-0': !isDisabled,
+        'opacity-0 transition-opacity duration-200 ease-out': !isOpen && !isDisabled,
+        'opacity-100 transition-opacity duration-300 ease-out delay-250': isOpen && !isDisabled,
       }"
     >
       <slot />
--- app/components/ui/List.vue
+++ app/components/ui/List.vue
@@ -15,30 +15,71 @@ const {
 const themeHoverClasses = computed(() => {
   switch (theme) {
     case 'blue':
-      return 'hover:bg-blue'
-    case 'green':
-      return 'hover:text-green'
-    case 'pink':
-      return 'hover:text-pink'
-    case 'purple':
-      return 'hover:text-purple'
+      return 'md:hover:bg-blue'
     default:
-      return 'hover:text-blue'
+      return ''
+  }
+})
+
+const themeActiveClasses = computed(() => {
+  switch (theme) {
+    case 'blue':
+      return 'max-md:bg-blue'
+    default:
+      return ''
+  }
+})
+
+const themeActiveTextClasses = computed(() => {
+  switch (theme) {
+    case 'blue':
+      return 'max-md:text-offblack'
+    default:
+      return ''
   }
 })
 
 const openIndex = ref<number | null>(null)
+const isHoverLocked = ref(false)
+const initialHoverIndex = ref<number | null>(null)
 
 const toggleItem = (index: number) => {
   openIndex.value = openIndex.value === index ? null : index
 }
 
 const isScreenMd = useAtMedia(getMediaQuery('md'))
+
+const handleItemEnter = (index: number) => {
+  if (!isScreenMd.value) {
+    return
+  }
+
+  if (!isHoverLocked.value) {
+    initialHoverIndex.value = index
+    isHoverLocked.value = true
+  }
+
+  if (initialHoverIndex.value !== null && index !== initialHoverIndex.value) {
+    initialHoverIndex.value = null
+  }
+}
+
+const handleListLeave = () => {
+  if (!isScreenMd.value) {
+    return
+  }
+
+  isHoverLocked.value = false
+  initialHoverIndex.value = null
+}
 </script>
 
 <template>
   <div class="">
-    <ul class="ui-list__list">
+    <ul
+      class="ui-list__list"
+      @mouseleave="handleListLeave"
+    >
       <li
         v-for="(item, index) in items"
         :key="item._uid"
@@ -46,10 +87,21 @@ const isScreenMd = useAtMedia(getMediaQuery('md'))
           ui-list__item
           group
           transition-[background-color]
-          duration-200
+          duration-(--hover-duration)
           ease-out
+          select-none
+          cursor-default
         "
-        :class="themeHoverClasses"
+        :class="[
+          themeHoverClasses,
+          {
+            [themeActiveClasses]: openIndex === index,
+          },
+        ]"
+        :style="{
+          '--hover-duration': isHoverLocked && initialHoverIndex !== index ? '0s' : '200ms',
+        }"
+        @mouseenter="handleItemEnter(index)"
         @click="!isScreenMd && toggleItem(index)"
       >
         <div
@@ -68,16 +120,70 @@ const isScreenMd = useAtMedia(getMediaQuery('md'))
               py-4
             "
           >
-            <span class="col-span-1 sm:col-span-2 md:col-span-3 type-h2 text-outline-current transition-[color,-webkit-text-stroke] duration-200 ease-out group-hover:text-offblack cursor-default">
+            <span
+              class="
+                col-span-1
+                sm:col-span-2
+                md:col-span-3
+                type-h2
+                text-outline-current
+                transition-[color,-webkit-text-stroke]
+                duration-(--hover-duration)
+                ease-out
+                md:group-hover:text-offblack
+              "
+              :class="{
+                [themeActiveTextClasses]: openIndex === index,
+              }"
+            >
               {{ (index + 1).toString().padStart(2, '0') }}
             </span>
 
-            <h3 class="col-span-3 sm:col-span-6 md:col-span-5 type-h2 text-outline-current transition-[color,-webkit-text-stroke] duration-200 ease-out group-hover:text-offblack cursor-default">
+            <h3
+              class="
+                col-span-3
+                sm:col-span-6
+                md:col-span-5
+                flex
+                items-center
+                justify-between
+                type-h2
+                text-outline-current
+                transition-[color,-webkit-text-stroke]
+                duration-(--hover-duration)
+                ease-out
+                md:group-hover:text-offblack
+              "
+              :class="{
+                [themeActiveTextClasses]: openIndex === index,
+              }"
+            >
               {{ item.title }}
+
+              <UiPlusMinus
+                class="md:hidden"
+                :is-open="openIndex === index"
+              />
             </h3>
 
-            <div class="col-start-2 col-span-3 sm:col-start-3 sm:col-span-6 md:col-span-4">
-              <UiExpandable :is-open="isScreenMd || openIndex === index">
+            <div
+              class="
+                col-start-2
+                col-span-3
+                sm:col-start-3
+                sm:col-span-6
+                md:col-span-4
+                md:opacity-0
+                md:transition-opacity
+                md:duration-(--hover-duration)
+                md:ease-out
+                group-hover:md:opacity-100
+              "
+            >
+              <UiExpandable
+                :is-open="openIndex === index"
+                :is-disabled="isScreenMd"
+              >
                 <p class="type-p max-md:pt-4">
                   {{ item.copy }}
                 </p>
@@ -92,12 +198,6 @@ const isScreenMd = useAtMedia(getMediaQuery('md'))
 
 <style scoped>
 @reference "@/assets/css/app.css";
-
-.ui-list__list {
-}
-
-.ui-list__item {
-}
 
 .ui-list__item-inner {
   border-top: 1px solid --alpha(currentColor / 20%);
