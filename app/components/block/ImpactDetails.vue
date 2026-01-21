7806617854def2342575ac754e95525bec10bf9e
Michael Pumo <hello@michaelpumo.com>
2026-01-21 14:45:18 +0000

    Impact stuff.

--- app/components/block/ImpactDetails.vue
+++ app/components/block/ImpactDetails.vue
@@ -7,18 +7,92 @@ interface Props {
 }
 
 const { block } = defineProps<Props>()
+const media = computed(() => block.media?.[0])
 </script>
 
 <template>
   <div
     v-editable="block"
-    class="grid gap-y-35"
+    class="grid gap-y-10 md:gap-y-20"
   >
     <UiContent
       :title="block.title"
       :headline="block.headline"
     />
 
-    <!-- <pre>{{ block }}</pre> -->
+    <div
+      class="wrapper-max grid grid-cols-(--app-grid) gap-x-(--app-inner-gutter) gap-y-10 md:gap-y-20"
+      :class="{
+        'grid-rows-[auto_auto] md:grid-rows-1': media && (storyblokRichTextContent(block.copy) || block.cta?.cached_url),
+      }"
+    >
+      <div
+        v-if="media"
+        class="col-start-1 col-span-full md:col-span-7 bg-[orange]"
+        :class="{
+          'max-md:row-start-2': media && (storyblokRichTextContent(block.copy) || block.cta?.cached_url),
+        }"
+      >
+        <NuxtImg
+          v-if="isImageComponent(media) && media.image?.filename && storyblokAssetType(media.image.filename) === 'image'"
+          class="block w-full h-auto"
+          :src="media.image.filename"
+          :alt="media.image.alt || ''"
+          :width="500"
+          :height="Math.round(storyblokImageDimensions(media.image.filename).height / storyblokImageDimensions(media.image.filename).width * 500)"
+          quality="85"
+          loading="lazy"
+          :modifiers="{
+            smart: true,
+          }"
+        />
+      </div>
+
+      <div
+        class="grid gap-10 col-start-2 sm:col-start-5 md:col-start-9 col-span-full"
+        :class="{
+          'max-md:row-start-1': media && (storyblokRichTextContent(block.copy) || block.cta?.cached_url),
+        }"
+      >
+        <div
+          v-if="storyblokRichTextContent(block.copy)"
+          class="ui-content__copy"
+        >
+          <StoryblokText :html="block.copy" />
+        </div>
+
+        <StoryblokLink
+          v-if="block.cta_title && block.cta?.cached_url"
+          class="inline-block"
+          :item="block.cta"
+        >
+          <UiButton>
+            {{ block.cta_title }}
+          </UiButton>
+        </StoryblokLink>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+@reference "@/assets/css/app.css";
+
+.ui-content__copy {
+  :deep(p),
+  :deep(li) {
+    @apply type-p;
+
+    text-wrap: pretty;
+    max-width: 24em;
+  }
+
+  :deep(p + p),
+  :deep(p + ul),
+  :deep(p + ol),
+  :deep(ul + p),
+  :deep(ol + p) {
+    padding-top: 1.25em;
+  }
+}
+</style>
--- app/components/ui/Content.vue
+++ app/components/ui/Content.vue
@@ -3,9 +3,10 @@ import type { StoryblokMultilink, StoryblokRichtext } from '@@/.storyblok/types/
 
 interface Props {
   title?: string
-  headline?: StoryblokRichtext | undefined
-  copy?: StoryblokRichtext | undefined
-  cta?: StoryblokMultilink | undefined
+  headline?: StoryblokRichtext
+  copy?: StoryblokRichtext
+  cta?: StoryblokMultilink
+  ctaTitle?: string
   ctaType?: 'solid' | 'outline'
 }
 
@@ -14,29 +15,16 @@ const {
   headline,
   copy,
   cta,
+  ctaTitle,
+  ctaType = 'solid',
 } = defineProps<Props>()
 </script>
 
 <template>
-  <div
-    class="
-      ui-content
-      wrapper-max
-      grid
-      grid-cols-(--app-grid)
-      gap-x-(--app-inner-gutter)
-      gap-y-10
-      md:gap-y-20
-    "
-  >
+  <div class="ui-content wrapper-max grid grid-cols-(--app-grid) gap-x-(--app-inner-gutter) gap-y-10 md:gap-y-20">
     <div
       v-if="title"
-      class="
-        ui-content__title
-        col-span-full
-        md:col-start-1
-        md:col-span-3
-      "
+      class="ui-content__title col-span-full md:col-start-1 md:col-span-3"
     >
       <p class="type-h5">
         {{ title }}
@@ -45,11 +33,7 @@ const {
 
     <div
       v-if="storyblokRichTextContent(headline)"
-      class="
-        ui-content__headline
-        col-span-full
-        md:col-span-9
-      "
+      class="ui-content__headline col-span-full md:col-span-9"
       :class="{
         'md:col-start-4': title,
         'md:col-start-2': !title,
@@ -60,11 +44,7 @@ const {
 
     <div
       v-if="storyblokRichTextContent(copy) || cta?.cached_url"
-      class="
-        ui-content__copy
-        grid
-        gap-10
-      "
+      class="ui-content__copy grid gap-10"
       :class="{
         'col-start-2 col-span-3 sm:col-start-5 sm:col-span-4 md:col-start-9 md:col-span-4 lg:col-start-9 lg:col-span-3': storyblokRichTextContent(copy),
         'col-start-1 col-span-full md:col-start-4': !storyblokRichTextContent(copy),
@@ -75,12 +55,12 @@ const {
       </div>
 
       <StoryblokLink
-        v-if="cta?.cached_url"
+        v-if="ctaTitle && cta?.cached_url"
         class="inline-block"
         :item="cta"
       >
         <UiButton :type="ctaType">
-          {{ cta.title }}
+          {{ ctaTitle }}
         </UiButton>
       </StoryblokLink>
     </div>
@@ -101,14 +81,19 @@ const {
 }
 
 .ui-content__copy {
-  :deep(p) {
+  :deep(p),
+  :deep(li) {
     @apply type-p;
 
     text-wrap: pretty;
     max-width: 24em;
   }
 
-  :deep(p + p) {
+  :deep(p + p),
+  :deep(p + ul),
+  :deep(p + ol),
+  :deep(ul + p),
+  :deep(ol + p) {
     padding-top: 1.25em;
   }
 }
