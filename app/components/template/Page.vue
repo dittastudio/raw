4cadc402897e83adfe846231c360dcb1c7ba1307
Michael Pumo <hello@michaelpumo.com>
2026-01-21 19:40:07 +0000

    Make better spacing.

--- app/components/template/Page.vue
+++ app/components/template/Page.vue
@@ -11,77 +11,72 @@ const { story } = defineProps<Props>()
 </script>
 
 <template>
-  <div class="w-full flex flex-col gap-20 md:gap-36">
-    <UiSection
-      v-for="block in story.content.blocks"
-      :key="block._uid"
-      :theme="'theme' in block && block.theme ? block.theme as Themes : undefined"
-      :class="[
-        'last:pb-20 last:md:pb-36',
-        {
-          'py-20 md:py-36': ['block_testimonials'].includes(block.component),
-        },
-      ]"
-    >
-      <BlockHero
-        v-if="block.component === 'block_hero'"
-        :block="block"
-      />
+  <UiSection
+    v-for="block in story.content.blocks"
+    :key="block._uid"
+    :theme="'theme' in block && block.theme ? block.theme as Themes : undefined"
+    :class="{
+      'py-20 md:py-36': !['block_hero', 'block_hero_brand'].includes(block.component),
+    }"
+  >
+    <BlockHero
+      v-if="block.component === 'block_hero'"
+      :block="block"
+    />
 
-      <BlockHeroBrand
-        v-else-if="block.component === 'block_hero_brand'"
-        :block="block"
-      />
+    <BlockHeroBrand
+      v-else-if="block.component === 'block_hero_brand'"
+      :block="block"
+    />
 
-      <BlockImpactDetails
-        v-else-if="block.component === 'block_impact_details'"
-        :block="block"
-      />
+    <BlockImpactDetails
+      v-else-if="block.component === 'block_impact_details'"
+      :block="block"
+    />
 
-      <BlockImpactStatement
-        v-else-if="block.component === 'block_impact_statement'"
-        :block="block"
-      />
+    <BlockImpactStatement
+      v-else-if="block.component === 'block_impact_statement'"
+      :block="block"
+    />
 
-      <BlockPosts
-        v-else-if="block.component === 'block_posts'"
-        :block="block"
-      />
+    <BlockPosts
+      v-else-if="block.component === 'block_posts'"
+      :block="block"
+    />
 
-      <BlockTeam
-        v-else-if="block.component === 'block_team'"
-        :block="block"
-      />
+    <BlockTeam
+      v-else-if="block.component === 'block_team'"
+      :block="block"
+    />
 
-      <BlockTestimonials
-        v-else-if="block.component === 'block_testimonials'"
-        :block="block"
-      />
+    <BlockTestimonials
+      v-else-if="block.component === 'block_testimonials'"
+      :block="block"
+    />
 
-      <BlockHoverList
-        v-else-if="block.component === 'block_hover_list'"
-        :block="block"
-      />
+    <BlockHoverList
+      v-else-if="block.component === 'block_hover_list'"
+      :block="block"
+    />
 
-      <BlockHoverGrid
-        v-else-if="block.component === 'block_hover_grid'"
-        :block="block"
-      />
+    <BlockHoverGrid
+      v-else-if="block.component === 'block_hover_grid'"
+      :block="block"
+    />
 
-      <BlockCarousel
-        v-else-if="block.component === 'block_carousel'"
-        :block="block"
-      />
+    <BlockCarousel
+      v-else-if="block.component === 'block_carousel'"
+      :block="block"
+    />
 
-      <BlockLogoWall
-        v-else-if="block.component === 'block_logo_wall'"
-        :block="block"
-      />
+    <BlockLogoWall
+      v-else-if="block.component === 'block_logo_wall'"
+      :block="block"
+    />
 
-      <BlockContact
-        v-else-if="block.component === 'block_contact'"
-        :block="block"
-      />
-    </UiSection>
-  </div>
+    <BlockContact
+      v-else-if="block.component === 'block_contact'"
+      :block="block"
+    />
+  </UiSection>
 </template>
