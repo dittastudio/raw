de6c81c8bed317f9e0934affcba1c9d17c0606b6
Michael Pumo <hello@michaelpumo.com>
2026-01-30 18:51:09 +0000

    Remove unused env.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,7 +57,6 @@ export default defineNuxtConfig({
     name: 'RAW',
   },
   runtimeConfig: {
-    STORYBLOK_MANAGEMENT_TOKEN: process.env.NUXT_STORYBLOK_MANAGEMENT_TOKEN,
     STORYBLOK_SPACE_ID: process.env.NUXT_STORYBLOK_SPACE_ID,
     MUX_ACCESS_TOKEN: process.env.NUXT_MUX_ACCESS_TOKEN,
     MUX_SECRET_KEY: process.env.NUXT_MUX_SECRET_KEY,
