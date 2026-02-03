7c6ec3b37a467b7fd2328170fe88656d304f567b
Michael Pumo <hello@michaelpumo.com>
2026-02-03 17:12:40 +0000

    feat(pastEvents): create past events query and block with card

---
+++ app/components/block/PastEvents.vue
@@ -0,0 +1,82 @@
+<script lang="ts" setup>
+import type { BlockPastEvents, Post } from '@@/.storyblok/types/289672313529140/storyblok-components'
+import type { ISbStoryData } from '@storyblok/js'
+
+interface Props {
+  block: BlockPastEvents
+}
+
+const { block } = defineProps<Props>()
+const storyblokApi = useStoryblokApi()
+
+interface EventPost {
+  uuid: string
+  name: string
+  full_slug: string
+  preview_image: Post['preview_image'] | Post['hero']
+  preview_text: Post['preview_text']
+  eventDatetime: Post['event_datetime']
+}
+
+const { data: posts } = await useAsyncData(() => `past-events`, async () => {
+  const today = new Date().toISOString().split('T')[0] // Format: YYYY-MM-DD
+
+  const { data } = await storyblokApi.get('cdn/stories', {
+    content_type: 'post',
+    page: 1,
+    per_page: 4,
+    sort_by: 'content.event_datetime:desc',
+    version: 'published',
+    excluding_fields: 'blocks,seo_title,seo_description,seo_image',
+    filter_query: {
+      category: {
+        in: 'events',
+      },
+      event_datetime: {
+        lt_date: `${today} 00:01`, // Only events before today
+      },
+    },
+  })
+
+  return data.stories as ISbStoryData<Post>[]
+}, {
+  transform: (payload: ISbStoryData<Post>[]): EventPost[] => {
+    const posts = payload.map(post => ({
+      uuid: post.uuid,
+      name: post.name,
+      full_slug: post.full_slug,
+      preview_image: post.content.preview_image?.filename ? post.content.preview_image : post.content.hero?.filename ? post.content.hero : undefined,
+      preview_text: post.content.preview_text,
+      eventDatetime: post.content.event_datetime,
+    }))
+
+    return posts
+  },
+})
+</script>
+
+<template>
+  <div
+    v-editable="block"
+    class="wrapper-max flex flex-col gap-15"
+  >
+    <p class="type-h5">
+      Past events
+    </p>
+
+    <ul class="grid grid-cols-2 lg:grid-cols-4 gap-x-(--app-inner-gutter) gap-y-(--app-outer-gutter)">
+      <li
+        v-for="event in posts"
+        :key="event.uuid"
+      >
+        <CardEvent
+          :slug="event.full_slug"
+          :image="event.preview_image"
+          :datetime="event.eventDatetime"
+          :headline="event.name"
+          :description="event.preview_text"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ app/components/card/Event.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import type { StoryblokAsset } from '@@/.storyblok/types/storyblok'
+
+interface Props {
+  slug?: string
+  image?: StoryblokAsset
+  datetime?: string
+  headline?: string
+  description?: string
+}
+
+const { slug, image, datetime, headline, description } = defineProps<Props>()
+</script>
+
+<template>
+  <article class="size-full">
+    <NuxtLink
+      class="flex flex-col items-start justify-start gap-6 size-full border-b border-(--app-text-color) pb-6"
+      :to="`/${slug}`"
+    >
+      <div class="w-full overflow-hidden aspect-video">
+        <NuxtImg
+          v-if="image?.filename && storyblokAssetType(image.filename) === 'image'"
+          class="block size-full object-cover"
+          :src="image.filename"
+          :alt="image.alt || headline || ''"
+          :width="500"
+          :height="Math.round(storyblokImageDimensions(image.filename).height / storyblokImageDimensions(image.filename).width * 500)"
+        />
+      </div>
+
+      <time
+        v-if="datetime"
+        class="type-h4 pb-6"
+      >
+        {{ formatDateDMY(datetime) }}
+      </time>
+
+      <h3
+        v-if="headline"
+        class="type-h5 text-balance max-w-[64ch]"
+      >
+        {{ headline }}
+      </h3>
+
+      <p
+        v-if="description"
+        class="type-p text-balance"
+      >
+        {{ description }}
+      </p>
+    </NuxtLink>
+  </article>
+</template>
