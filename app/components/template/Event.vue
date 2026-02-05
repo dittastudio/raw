eb32968cfc51d089d4e2111c67efb148595d3acc
Michael Pumo <hello@michaelpumo.com>
2026-02-05 16:08:35 +0000

    feat(event): add Event component and related types, update story handling

---
+++ app/components/template/Event.vue
@@ -0,0 +1,116 @@
+<script lang="ts" setup>
+import type { Event } from '@@/.storyblok/types/289672313529140/storyblok-components'
+import type { Themes } from '@@/types/app'
+import type { ISbStoryData } from '@storyblok/js'
+
+interface Props {
+  story: ISbStoryData<Event>
+}
+
+const { story } = defineProps<Props>()
+
+const isHeroBlock = (index: number, indexFrom: number) => {
+  return ['block_hero'].includes(story.content.blocks?.[index + indexFrom]?.component ?? '')
+}
+
+const isLastBlock = (index: number) => {
+  return index === (story.content.blocks?.length ?? 0) - 1 && !isHeroBlock(index, 0)
+}
+</script>
+
+<template>
+  <UiTheme
+    v-for="(block, index) in story.content.blocks"
+    :key="block._uid"
+    :theme="'theme' in block ? (block.theme as Themes) : undefined"
+  >
+    <section
+      :class="{
+        'py-(--app-vertical-spacing)': !isHeroBlock(index, 0),
+        'pt-[calc(var(--app-vertical-spacing)*1.5)]': isHeroBlock(index, -1),
+        'pb-[calc(var(--app-vertical-spacing)*1.5)]': isHeroBlock(index, 1) || isLastBlock(index),
+      }"
+    >
+      <BlockBCorp
+        v-if="block.component === 'block_bcorp'"
+        :block="block"
+      />
+
+      <BlockCarousel
+        v-else-if="block.component === 'block_carousel'"
+        :block="block"
+      />
+
+      <BlockGallery
+        v-else-if="block.component === 'block_gallery'"
+        :block="block"
+      />
+
+      <BlockHero
+        v-else-if="block.component === 'block_hero'"
+        :block="block"
+        :name="story.name"
+      />
+
+      <BlockHoverGrid
+        v-else-if="block.component === 'block_hover_grid'"
+        :block="block"
+      />
+
+      <BlockHoverList
+        v-else-if="block.component === 'block_hover_list'"
+        :block="block"
+      />
+
+      <BlockImpactDetails
+        v-else-if="block.component === 'block_impact_details'"
+        :block="block"
+      />
+
+      <BlockImpactStatement
+        v-else-if="block.component === 'block_impact_statement'"
+        :block="block"
+      />
+
+      <BlockLogoWall
+        v-else-if="block.component === 'block_logo_wall'"
+        :block="block"
+      />
+
+      <BlockMedia
+        v-else-if="block.component === 'block_media'"
+        :block="block"
+      />
+
+      <BlockNextEvent
+        v-else-if="block.component === 'block_next_event'"
+        :block="block"
+      />
+
+      <BlockPastEvents
+        v-else-if="block.component === 'block_past_events'"
+        :block="block"
+      />
+
+      <BlockSplitText
+        v-else-if="block.component === 'block_split_text'"
+        :block="block"
+      />
+
+      <BlockTestimonials
+        v-else-if="block.component === 'block_testimonials'"
+        :block="block"
+      />
+
+      <BlockText
+        v-else-if="block.component === 'block_text'"
+        :block="block"
+      />
+
+      <BlockTicker
+        v-else-if="block.component === 'block_ticker'"
+        :block="block"
+      />
+    </section>
+  </UiTheme>
+</template>
