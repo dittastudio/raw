f5caf32e1c44ea2e188385341f63ae48c5d7d43d
Michael Pumo <hello@michaelpumo.com>
2026-02-18 15:30:15 +0000

    feat(Person, Team): add LinkedIn icon and implement GSAP animations for team members

--- app/components/ui/Team.vue
+++ app/components/ui/Team.vue
@@ -2,6 +2,10 @@
 import type { Person } from '#storyblok-components'
 import type { Themes } from '@@/types/app'
 import type { ISbStoryData } from '@storyblok/js'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 interface Props {
   title?: string
@@ -11,10 +15,45 @@ interface Props {
 
 const { title, people, accent } = defineProps<Props>()
 const peoples = computed(() => people?.filter(person => typeof person !== 'string'))
+const container = useTemplateRef('container')
+
+onMounted(() => {
+  const items = container.value?.querySelectorAll('li')
+
+  if (!items?.length) {
+    return
+  }
+
+  gsap.fromTo(items, {
+    opacity: 0,
+    scale: 0.75,
+    filter: 'blur(5px)',
+    transformOrigin: 'left center',
+  }, {
+    opacity: 1,
+    scale: 1,
+    filter: 'blur(0px)',
+    duration: 1.5,
+    ease: 'expo.out',
+    stagger: {
+      amount: 0.9,
+    },
+    scrollTrigger: {
+      trigger: container.value,
+      start: 'top 80%',
+      end: 'bottom 20%',
+      scrub: 1,
+      markers: false,
+    },
+  })
+})
 </script>
 
 <template>
-  <div class="@container grid gap-12">
+  <div
+    ref="container"
+    class="@container grid gap-12"
+  >
     <h3
       v-if="title"
       class="type-h5"
