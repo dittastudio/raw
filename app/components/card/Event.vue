627890e844ce41de4d1c5f2350485c34217848b1
Will The Dev <hello@willthe.dev>
2026-03-17 14:58:07 +0000

    fix(events): fix image opt

--- app/components/card/Event.vue
+++ app/components/card/Event.vue
@@ -1,15 +1,12 @@
 <script lang="ts" setup>
-import type { StoryblokAsset } from '#storyblok-types'
-
 interface Props {
   slug?: string
-  image?: StoryblokAsset
   datetime?: string
   headline?: string
   description?: string
 }
 
-const { slug, image, datetime, headline, description } = defineProps<Props>()
+const { slug, datetime, headline, description } = defineProps<Props>()
 </script>
 
 <template>
@@ -19,15 +16,7 @@ const { slug, image, datetime, headline, description } = defineProps<Props>()
       :to="`/${slug}`"
     >
       <div class="w-full overflow-hidden aspect-video">
-        <NuxtImg
-          v-if="image?.filename && storyblokAssetType(image.filename) === 'image'"
-          class="block size-full object-cover"
-          :src="image.filename"
-          :alt="image.alt || headline || ''"
-          :width="500"
-          :height="Math.round(storyblokImageDimensions(image.filename).height / storyblokImageDimensions(image.filename).width * 500)"
-          loading="lazy"
-        />
+        <slot name="image" />
       </div>
 
       <time
