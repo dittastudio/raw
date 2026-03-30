70275a8d5b2edc809249193d51a6b75820fe619a
Michael Pumo <hello@michaelpumo.com>
2026-03-30 12:38:04 +0100

    refactor(app store): state management for data capture

--- app/stores/app.ts
+++ app/stores/app.ts
@@ -2,22 +2,34 @@ import type { Themes } from '@@/types/app'
 import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', () => {
+  // Cookies.
   const cookieConsent = ref<'accepted' | 'declined' | null>(null)
   const hasDecidedCookies = computed(() => cookieConsent.value !== null)
-  const isHeaderOpen = ref(false)
-  const activeTheme = ref<Themes>('light')
 
   function setAcceptedCookies(value: boolean) {
     cookieConsent.value = value ? 'accepted' : 'declined'
+  }
+
+  // Data capture.
+  const dataCaptured = ref(false)
+
+  function setDataCaptured(value: boolean) {
+    dataCaptured.value = value
+  }
+
+  // Header.
+  const isHeaderOpen = ref(false)
+
+  function toggleHeaderMenu() {
+    isHeaderOpen.value = !isHeaderOpen.value
   }
 
   function setHeaderMenu(value: boolean) {
     isHeaderOpen.value = value
   }
 
-  function toggleHeaderMenu() {
-    isHeaderOpen.value = !isHeaderOpen.value
-  }
+  // Theme.
+  const activeTheme = ref<Themes>('light')
 
   function setActiveTheme(value: Themes) {
     activeTheme.value = value
@@ -27,6 +39,8 @@ export const useAppStore = defineStore('app', () => {
     cookieConsent,
     hasDecidedCookies,
     setAcceptedCookies,
+    dataCaptured,
+    setDataCaptured,
     isHeaderOpen,
     setHeaderMenu,
     toggleHeaderMenu,
@@ -35,7 +49,7 @@ export const useAppStore = defineStore('app', () => {
   }
 }, {
   persist: {
-    pick: ['cookieConsent'],
+    pick: ['cookieConsent', 'dataCaptured'],
     storage: piniaPluginPersistedstate.cookies(),
   },
 })
